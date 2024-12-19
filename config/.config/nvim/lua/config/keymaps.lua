e411a389078af12fc3dabaab9de1c77d7e762ad2
Kavindu Jayarathne <kavindujayarathne.dev@gmail.com>
2024-12-20 03:17:21 +0530

    chore(config): update keymaps for Neovim

--- config/.config/nvim/lua/config/keymaps.lua
+++ config/.config/nvim/lua/config/keymaps.lua
@@ -1,21 +1,21 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-local map = vim.keymap.set
+-- Keykeymaps are automatically loaded on the VeryLazy event
+-- Default keykeymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keykeymaps.lua
+-- Add any additional keykeymaps here
+local keymap = vim.keymap.set
 
 -- Replacing the <Esc> with other keybindings for ease of use
-map("i", "jj", "<Esc>", { desc = "Exit Insert Mode" })
+keymap("i", "jj", "<Esc>", { desc = "Exit Insert Mode" })
 
 -- Move Lines
--- map("n", "<C-S-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
--- map("n", "<C-S-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
--- map("i", "<C-S-n>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
--- map("i", "<C-S-m>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
--- map("v", "<C-S-j>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
--- map("v", "<C-S-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
+-- keymap("n", "<C-S-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
+-- keymap("n", "<C-S-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+-- keymap("i", "<C-S-n>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+-- keymap("i", "<C-S-m>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+-- keymap("v", "<C-S-j>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+-- keymap("v", "<C-S-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
 
 -- Swap Windows Around
--- map("n", "<leader>wh", "<C-w>H", { desc = "Move window to the left" })
--- map("n", "<leader>wl", "<C-w>L", { desc = "Move window to the right" })
--- map("n", "<leader>wk", "<C-w>K", { desc = "Move window to the top" })
--- map("n", "<leader>wj", "<C-w>J", { desc = "Move window to the bottom" })
+keymap("n", "<leader>wh", "<C-w>H", { desc = "Move window to the left" })
+keymap("n", "<leader>wl", "<C-w>L", { desc = "Move window to the right" })
+keymap("n", "<leader>wk", "<C-w>K", { desc = "Move window to the top" })
+keymap("n", "<leader>wj", "<C-w>J", { desc = "Move window to the bottom" })
