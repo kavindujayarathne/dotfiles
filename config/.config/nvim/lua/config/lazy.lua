19f0c7cc8d9c644ff2d7677698cd638322750daf
Kavindu Jayarathne <kavindujayarathne@proton.me>
2026-05-08 08:45:44 +0530

    chore: update

--- config/.config/nvim/lua/config/lazy.lua
+++ config/.config/nvim/lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.astro" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
 
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
 
