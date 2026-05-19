0ee6199da0e6e66154509410d66e07798e84bdac
Kavindu Jayarathne <kavindujayarathne@proton.me>
2026-05-20 04:57:48 +0530

    chore: update

--- config/.config/nvim/lua/plugins/disable.lua
+++ config/.config/nvim/lua/plugins/disable.lua
@@ -2,4 +2,5 @@ return {
   { "folke/tokyonight.nvim", enabled = false },
   { "zbirenbaum/copilot.lua", enabled = false },
   { "yetone/avante.nvim", enabled = false },
+  { "laytan/cloak.nvim", enabled = false },
 }
